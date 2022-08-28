95d728842e7c5b8835f30c0222408cd79d70cb12
unknown <rakkakeren@gmail.com>
2022-08-29 03:10:56 +0700

    rename header and about

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-container">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Recognize Machine Learning</h1>
+    <p>Where machine learning is taught concisely</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderPage',
+    
+  }
+</script>
+
+<style scoped>
+  h1 {
+    text-transform: capitalize;
+  }
+
+  .header-container {
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/views/CategoriesView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="categories">
+    <h1>Categories</h1>
+  </div>
+</template>
