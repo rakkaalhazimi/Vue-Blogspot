5b179a3ded0d8cd6a2321efa6347f5ccc3873bf2
unknown <rakkakeren@gmail.com>
2022-08-29 03:08:13 +0700

    add window in global vue

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+let app = createApp(App).use(router)
+app.mount('#app')
+
+// Add window global
+app.config.globalProperties.window = window
