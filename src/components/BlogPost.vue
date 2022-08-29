4356a2104b79b0635d1a75a2208a9584cef32d8d
unknown <rakkakeren@gmail.com>
2022-08-29 22:40:50 +0700

    add blog post compoenents

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="blog-post">
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BlogPost',
+    props: ['title', 'content']
+  }
+</script>
+
+<style></style>
