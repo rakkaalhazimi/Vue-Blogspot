dd539aa9db6930b4cfde6d0de7192cdde741f9ae
unknown <rakkakeren@gmail.com>
2022-08-29 22:38:38 +0700

    resolve url path

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CategoriesView.vue')
+  },
+  {
+    path: '/post/:id',
+    name: 'post',
+    component: () => import('../views/CategoriesView.vue')
   }
 ]
 
