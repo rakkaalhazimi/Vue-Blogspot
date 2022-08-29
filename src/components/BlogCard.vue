5149dd090d28b1ce23ab36562233829d4a4c8aa6
unknown <rakkakeren@gmail.com>
2022-08-29 21:55:15 +0700

    integrate data to blogCard

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="blog-card">
     <img src="../assets/logo.png" alt="Blog picture"/>
-    <h3>Blog Title</h3>
-    <p>Blog Description</p>
-    <p>#tags</p>
+    <h3>{{ title }}</h3>
+    <p>{{ desc }}</p>
+    <p>{{ tags }}</p>
   </div>
 
 </template>
 
 <script>
    export default {
-    name: 'BlogCard'
+    name: 'BlogCard',
+    props: ['title', 'desc', 'tags']
   }
 </script>
--- src/components/BlogCardList.vue
+++ src/components/BlogCardList.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="blog-card-list">
-    <BlogCard v-for="n in 10" :key="n"/>
+    <BlogCard 
+      v-for="blogPost in blogPosts" 
+      :key="blogPost.id"
+      :title="blogPost.title"
+      :desc="blogPost.desc"
+      :tags="blogPost.tags"
+    />
   </div>
 </template>
 
 <script>
   import BlogCard from "./BlogCard.vue"
 
+  let blogPosts = [
+    {title: "Blog#1 Title", desc: "Blog Descriptions", tags: "#Tags", id: 1},
+    {title: "Blog#2 Title", desc: "Blog Descriptions", tags: "#Tags", id: 2},
+    {title: "Blog#3 Title", desc: "Blog Descriptions", tags: "#Tags", id: 3},
+    {title: "Blog#4 Title", desc: "Blog Descriptions", tags: "#Tags", id: 4},
+  ]
+
   export default {
     name: 'BlogCardList',
-    components : {BlogCard}
+    components : {BlogCard},
+    data() {return {blogPosts: blogPosts}}
   }
 </script>
-
-<style>
-
-</style>
