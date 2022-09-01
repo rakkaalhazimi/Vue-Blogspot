b59a38b0506c4f9b157e081a8df329b4c1371d47
unknown <rakkakeren@gmail.com>
2022-09-01 11:04:37 +0700

    add additional options to editor

--- src/components/Blog/BlogEditor.vue
+++ src/components/Blog/BlogEditor.vue
@@ -9,15 +9,26 @@ import "quill/dist/quill.core.css"
 import "quill/dist/quill.snow.css"
 import "quill/dist/quill.bubble.css"
 
+
 onMounted(() => {
-  let options = {theme: "snow"}
+  let options = {
+    theme: "snow",
+    modules: { toolbar: {} },
+    placeholder: "Enter text here..."
+  }
+
+
   let editor = new Quill("#editor", options)
-  editor.on("text-change", () => {})
+  editor.on("text-change", () => { })
 })
 
 
 </script>
 
 <style>
-
+#editor,
+.ql-toolbar {
+  background-color: var(--complement);
+  color: var(--dominant);
+}
 </style>
