ddcd622e92d591fe814b601ef717623258625f01
unknown <rakkakeren@gmail.com>
2022-09-02 17:22:33 +0700

    add publish button component

---
+++ src/components/Buttons/PublishButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="post-publish-btn">Publish</button>
+</template>
+
+<style>
+  .post-publish-btn {
+    padding: var(--medium-padding);
+    background-color: var(--complement);
+    color: var(--dominant);
+    cursor: pointer;
+  }
+</style>
