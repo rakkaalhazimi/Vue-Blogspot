2883c4e493f8cba2d786642372ee0ce4f66d8691
unknown <rakkakeren@gmail.com>
2022-09-02 21:31:04 +0700

    remove vue font-awesome

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import FontAwesomeIcon from './icons'
 
 let app = createApp(App).use(router)
 app.mount('#app')
 
 // Register font awesome components
-app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 // Add window global
 app.config.globalProperties.window = window
