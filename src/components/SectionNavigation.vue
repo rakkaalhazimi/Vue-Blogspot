8109a49118d97aee18a2e43d339570a6ad79e3c2
unknown <rakkakeren@gmail.com>
2022-09-03 11:57:00 +0700

    add about navigation and router

--- src/components/SectionNavigation.vue
+++ src/components/SectionNavigation.vue
@@ -1,6 +1,7 @@
 <template>
   <nav ref="navbar">
     <router-link to="/">Home</router-link>
-    <router-link to="/about">Categories</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/categories">Categories</router-link>
   </nav>
 </template>
