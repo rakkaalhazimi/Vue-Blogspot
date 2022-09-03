89f54796670224947be2423345818b1571f4b4f0
unknown <rakkakeren@gmail.com>
2022-09-03 12:23:01 +0700

    add header blog editor

---
+++ src/components/HeaderBlogEditor.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <h1>Add Post</h1>
+    <p>Enter a title, date and content.</p>
+    <p>Make sure to see the post preview before publish.</p>
+  </header>
+</template>
--- src/views/BlogEditorView.vue
+++ src/views/BlogEditorView.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="add-blog" class="content-view">
+    <HeaderBlogEditor />
     <BlogEditor v-once />
   </div>
 </template>
 
 <script setup>
 import BlogEditor from '@/components/BlogEditor.vue';
+import HeaderBlogEditor from '@/components/HeaderBlogEditor.vue';
 </script>
