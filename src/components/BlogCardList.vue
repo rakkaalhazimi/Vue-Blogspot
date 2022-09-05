3dad6b002ec2910c5e37dd324361f06285d1dd77
unknown <rakkakeren@gmail.com>
2022-09-05 21:35:42 +0700

    add: computed filtered posts based on keywords

--- src/components/BlogCardList.vue
+++ src/components/BlogCardList.vue
@@ -1,14 +1,20 @@
 <template>
+  <p>{{ keywords }}</p>
   <div class="blog-card-list">
-    <BlogCard v-for="blogPost in blogPosts" :key="blogPost.id" :title="blogPost.title" :desc="blogPost.desc"
-      :tags="blogPost.tags" />
+    <BlogCard v-for="blogPost in blogPostsFiltered" :key="blogPost.id" :post="blogPost" />
   </div>
 </template>
 
 <script setup>
-import { inject } from "vue";
+import { computed, defineProps, inject } from "vue";
 import BlogCard from "./BlogCard.vue"
+
+const props = defineProps(["keywords"])
 
 // Receive blog post from root App
 let blogPosts = inject("blogPosts")
+
+let blogPostsFiltered = computed(() => 
+  blogPosts.filter((post) => { return post.title.toLowerCase().includes(props.keywords.toLowerCase()) }
+))
 </script>
