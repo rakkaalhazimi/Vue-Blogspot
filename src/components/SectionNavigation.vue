59c5dd2adf02d22b34699c8437db047afb53677e
unknown <rakkakeren@gmail.com>
2022-09-06 18:37:55 +0700

    style: specify navbar class and set backdrop

--- src/components/SectionNavigation.vue
+++ src/components/SectionNavigation.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
-    <h1>ALHAZIMI</h1>
-    <ul>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/projects">Projects</router-link></li>
+    <h1 class="nav-logo">ALHAZIMI</h1>
+    <ul class="nav-link-container">
+      <li class="nav-link-item"><router-link to="/">Home</router-link></li>
+      <li class="nav-link-item"><router-link to="/about">About</router-link></li>
+      <li class="nav-link-item"><router-link to="/projects">Projects</router-link></li>
     </ul>
   </nav>
 </template>
