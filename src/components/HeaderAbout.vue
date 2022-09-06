0be5ec767bf27b736ca7eac868711974fd21ebe8
unknown <rakkakeren@gmail.com>
2022-09-06 16:50:06 +0700

    style: specify about header class
    also add text style and padding

--- src/components/HeaderAbout.vue
+++ src/components/HeaderAbout.vue
@@ -1,8 +1,13 @@
 <template>
-  <header>
+  <header class="header-about">
     <img src="../assets/photo.jpg" />
-    <h2>Rakka Alhazimi</h2>
-    <p>Data Science, Web Developer</p>
-    <p>Descriptions</p>
+    <h2 class="about-name">Rakka Alhazimi</h2>
+    <p class="about-title">Data Science, Web Developer</p>
+    <p class="about-desc">
+      Proficient Python Programmer with 3 years learning experience.
+      Machine Learning Lecturer from one of the Educational Technology startups.
+      Have a tons of experiences doing client requested projects like machine learning,
+      web prototype and browser automation.
+    </p>
   </header>
 </template>
