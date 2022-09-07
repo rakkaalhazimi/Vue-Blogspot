840ff8ac280c2a7798eb328c0568ab19ab6d96ab
unknown <rakkakeren@gmail.com>
2022-09-07 20:29:40 +0700

    delete: search.js

--- src/stores/search.js
+++
@@ -1,9 +0,0 @@
-import { ref } from "vue"
-import { defineStore } from "pinia"
-
-function searchStore() {
-  const keywords = ref("")
-  return { keywords }
-}
-
-export const useSearchStore = defineStore("search", searchStore)
