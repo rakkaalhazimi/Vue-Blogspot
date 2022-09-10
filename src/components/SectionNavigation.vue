0ae3848c3cdd37d8a3a17a2f1874eb6429a397da
unknown <rakkakeren@gmail.com>
2022-09-10 07:26:56 +0700

    change: use nav-link class instead locator
    add login button

--- src/components/SectionNavigation.vue
+++ src/components/SectionNavigation.vue
@@ -5,9 +5,10 @@
       <li class="nav-link-item">
         <button class="nav-close-btn" @click="hideNavbar"><i class="fa-solid fa-xmark"></i></button>
       </li>
-      <li class="nav-link-item"><router-link to="/">Home</router-link></li>
-      <li class="nav-link-item"><router-link to="/about">About</router-link></li>
-      <li class="nav-link-item"><router-link to="/projects">Projects</router-link></li>
+      <li class="nav-link-item"><router-link to="/" class="nav-link" >Home</router-link></li>
+      <li class="nav-link-item"><router-link to="/about" class="nav-link" >About</router-link></li>
+      <li class="nav-link-item"><router-link to="/projects" class="nav-link" >Projects</router-link></li>
+      <li class="nav-link-item"><button class="nav-link login-btn">Login</button></li>
     </ul>
     <button class="nav-menu-bars" @click="showNavbar">
       <i class="fa-solid fa-bars"></i>
