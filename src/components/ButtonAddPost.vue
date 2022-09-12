afabb3baa4cd79c1f4dda772dd664a97cb97e24e
unknown <rakkakeren@gmail.com>
2022-09-13 03:12:41 +0700

    delete: unused button components

--- src/components/ButtonAddPost.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <router-link to="post/create">
-    <button class="add-blog-btn">Add Blog</button>
-  </router-link>
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,16 @@
     <BlogCoverage />
     <SearchBar />
     <BlogBrowser />
-    <ButtonAddPost v-if="isLogin" />
 
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+// import { ref } from 'vue';
 import HeaderHome from '@/components/HeaderHome.vue';
 import BlogCoverage from '@/components/BlogCoverage.vue';
-import ButtonAddPost from '@/components/ButtonAddPost.vue';
 import BlogBrowser from '@/components/BlogBrowser.vue';
 
-let isLogin = ref(false)
+// let isLogin = ref(false)
 
 </script>
