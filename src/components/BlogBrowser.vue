c2996326a32e3108dca523b7f297c38f9be215e9
unknown <rakkakeren@gmail.com>
2022-09-12 15:21:34 +0700

    change: move blog list to blog browser

--- src/components/BlogBrowser.vue
+++ src/components/BlogBrowser.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="blog-browse" class="content-pad">
     <SearchBar />
+    <BlogCardContainer />
   </div>
 </template>
 
 <script setup>
+import BlogCardContainer from '@/components/BlogCardContainer.vue';
 import SearchBar from "./SearchBar.vue";
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="home" class="content-view">
-    <HeaderHome/>
+    <HeaderHome />
     <BlogCoverage />
     <SearchBar />
     <BlogBrowser />
-    <ButtonAddPost v-if="isLogin"/>
-    <BlogCardContainer />
+    <ButtonAddPost v-if="isLogin" />
+
   </div>
 </template>
 
@@ -13,7 +13,6 @@
 import { ref } from 'vue';
 import HeaderHome from '@/components/HeaderHome.vue';
 import BlogCoverage from '@/components/BlogCoverage.vue';
-import BlogCardContainer from '@/components/BlogCardContainer.vue';
 import ButtonAddPost from '@/components/ButtonAddPost.vue';
 import BlogBrowser from '@/components/BlogBrowser.vue';
 
