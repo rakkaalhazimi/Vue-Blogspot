b3b5a9e1f35eefa21094ee1fa7e42ed4817444dd
unknown <rakkakeren@gmail.com>
2022-09-18 11:48:54 +0700

    change: refactor header about

--- src/components/HeaderAbout.vue
+++ src/components/HeaderAbout.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="header-about">
-    <img class="about-photo" src="../assets/photo.jpg" />
+    <img class="about-photo medium-low-mb" src="../assets/photo.jpg" />
     <h2 class="about-name">Rakka Alhazimi</h2>
-    <p class="about-title">Data Scientist, Web Developer</p>
+    <p class="about-title medium-high-mb">Data Scientist, Web Developer</p>
     <p class="about-desc">
       Proficient Python Programmer with 3 years learning experience.
       Machine Learning Lecturer from one of the Educational Technology startups.
-      Have a tons of experiences doing client requested projects like machine learning,
+      Have a tons of experiences doing client projects on machine learning,
       web prototype and browser automation.
     </p>
   </header>
@@ -20,7 +20,6 @@ header.header-about {
 
 
 .about-photo {
-  margin: 0 auto;
   width: 150px;
   height: 150px;
   border-radius: 100px;
@@ -33,25 +32,20 @@ header.header-about {
 }
 
 .about-title {
-  margin-bottom: 3rem;
   color: $accent;
 }
 
 .about-desc {
   letter-spacing: 0.9px;
   line-height: 20px;
-}
 
-@media (min-width: 768px) {
-  .about-desc {
+  @media (min-width: 768px) {
     padding: 0 10vw;
   }
-}
 
-@media (min-width: 1200px) {
-  .about-desc {
+  @media (min-width: 1200px) {
     padding: 0 20vw;
-  }
 
+  }
 }
 </style>
